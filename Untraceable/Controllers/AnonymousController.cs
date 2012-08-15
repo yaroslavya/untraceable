ea32b90791e79fec452da359aa511b663946940a
Yaroslav Yakovlev <yaroslavya@gmail.com>
2012-08-15 21:01:55 +0800

    initial commit

---
+++ Untraceable/Controllers/AnonymousController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Untraceable.Controllers
+{
+    /// <summary>
+    /// All the reposonses of this controller should be anonymous in terms of 
+    /// technology, like asp.net version, server etc.
+    /// </summary>
+    public class AnonymousController : Controller
+    {
+        //
+        // GET: /Anonymous/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
